d0fd659e4a4085a0557fc24bb28b3ff8e3f080d1
EgemenEsen <79070462+eqemenesen@users.noreply.github.com>
2024-02-02 05:58:42 +0300

    some magic

--- src/main/java/com/newche/BackJobScheduler.java
+++ src/main/java/com/newche/BackJobScheduler.java
@@ -7,26 +7,28 @@ import org.springframework.stereotype.Component;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-@Component // This makes the class a Spring-managed bean
-public class BackJobScheduler{
+@Component
+public class BackJobScheduler {
     Logger logger = LoggerFactory.getLogger(BackJobScheduler.class);
 
-    @Scheduled(cron = "0 5 * * * *") // Runs every hour
+    @Scheduled(cron = "0 5 * * * *")
     public void runDailyTasks() {
         logger.info("Daily - Backjobs Started...");
 
         executeTask("src/main/resources/python/webScraping/sciencealert_ws.py", "Science Alert", "Daily");
         executeTask("src/main/resources/python/webScraping/spacenews_ws.py", "Space News", "Daily");
+        executeTask("src/main/resources/python/webScraping/motorsport_ws.py", "Motorsport", "Daily");
         
         logger.info("Daily - Backjobs END...");
     }
 
-    @Scheduled(fixedDelay = Long.MAX_VALUE, initialDelay = 5000) // Runs once at startup
+    @Scheduled(fixedDelay = Long.MAX_VALUE, initialDelay = 5000)
     public void runStartupTasks() {
         logger.info("Startup - Backjobs Started...");
 
         executeTask("src/main/resources/python/webScraping/sciencealert_ws.py", "Science Alert", "StartUp");
-        executeTask("src/main/resources/python/webScraping/spacenews_ws.py", "Space News", "StarUp");
+        executeTask("src/main/resources/python/webScraping/spacenews_ws.py", "Space News", "StartUp");
+        executeTask("src/main/resources/python/webScraping/motorsport_ws.py", "Motorsport", "StartUp");
 
         logger.info("Startup - Backjobs END...");
     }
@@ -34,7 +36,7 @@ public class BackJobScheduler{
     private void executeTask(String scriptPath, String taskName, String timeTask) {
         logger.info("{} - Executing task for: {}...", timeTask, taskName);
         runPythonScript(scriptPath);
-        logger.info("{} - Task completed for: {}...",timeTask, taskName);
+        logger.info("{} - Task completed for: {}...", timeTask, taskName);
     }
 
     private void runPythonScript(String scriptPath) {
@@ -46,14 +48,13 @@ public class BackJobScheduler{
             try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
                 String line;
                 while ((line = reader.readLine()) != null) {
-                    System.out.println(line);
+                    logger.debug(line); 
                 }
                 int exitCode = process.waitFor();
-                System.out.println("Exited with code: " + exitCode);
+                logger.info("Exited with code: {}", exitCode); // Use logger with placeholder
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error("An error occurred while running the Python script: ", e);
         }
     }
-    
 }
